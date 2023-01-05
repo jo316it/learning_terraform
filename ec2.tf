fb3f6623da6a0d04585ac02d1f4b70447cbfef12
jo316it <ricardofromit@gmail.com>
2023-01-05 15:04:40 -0300

    falta criar vpc

--- ec2.tf
+++ ec2.tf
@@ -8,11 +8,27 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
+resource "aws_key_pair" "this" {
+  key_name   = "fmt-keypar"
+  public_key = file("./fmt-keypair.pub")
+
+}
+
+
 
 resource "aws_instance" "server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   count         = 4
+  user_data     = <<-EOF
+                #! /bin/bash
+                sudo apt update  \
+                curl https://releases.rancher.com/install-docker/19.03.sh | sh \
+                usermod -aG docker ubuntu
+                EOF
+  key_name      = aws_key_pair.this.key_name
+
+
 
   tags = {
     Name        = "server-${count.index}"
@@ -20,4 +36,11 @@ resource "aws_instance" "server" {
     Provisioner = "Terraform"
     Repo        = var.repo
   }
+
+
+
 }
+
+
+
+
